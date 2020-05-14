fa7de606c544254a36c071eb5f89ecef98f14ede
NehaVerma <neha0305verma@gmail.com>
2020-05-14 12:55:21 +0530

    mock response change

--- course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/course/CourseManagementActor.java
+++ course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/course/CourseManagementActor.java
@@ -26,12 +26,9 @@ public class CourseManagementActor extends BaseActor {
 
     private void createCourse(Request request) {
         Response response = new Response();
-        if(request.getRequest().containsKey("source")){
-            response.put("do_11301654129459200011", "do_1234");
-        } else {
-            response.put("course_id", "do_1234");
-            response.put("versionKey", "1589374765530");
-        }
+        response.put("course_id", "do_1234");
+        response.put("identifier", "do_1234");
+        response.put("versionKey", "1589374765530");
         sender().tell(response, self());
     }
 }
